d9b7858c66b63353951f78bd17fce1ee6658b5f4
Akshata Satpute <140546785+akshatasatpute@users.noreply.github.com>
2024-06-25 16:31:06 +0530

    Update Stemcheck_F.py

--- Stemcheck_F.py
+++ Stemcheck_F.py
@@ -66,7 +66,7 @@ def get_dataset(selected_assignment_file):
     return file_mapping[selected_assignment_file]
 
 # Define the folder path for CSV files
-folder_path = r"https://github.com/akshatasatpute/Stemcheck/tree/main/Files"
+folder_path = 'https://github.com/akshatasatpute/Stemcheck/tree/main/Files'
 
 # Read all assignment files from the folder
 file_mapping = read_assignment_files(folder_path)
